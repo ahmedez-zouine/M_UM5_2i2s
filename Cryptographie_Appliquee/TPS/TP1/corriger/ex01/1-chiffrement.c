45ed6d94fac600d9c5c005fbad7b9a5e8a00299a
Goodman <=>
2024-11-28 01:19:17 +0100

    Done

---
+++ Cryptographie_Appliquee/TPS/TP1/corriger/ex01/1-chiffrement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+    int size = strlen(argv[1]);
+    //int key = atoi(argv[2]);
+    
+    while (i < size) {
+        char c = argv[1][i];
+
+        c = 'A' + (c - 'A' + 5) % 26;
+        write(1, &c, 1);
+        i++;
+    }
+    write(1, "\n", 1);
+    return 0;
+}
