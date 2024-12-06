0eb10da6711164e91cd08d25040a7932924e6517
Goodman <=>
2024-12-06 21:01:46 +0100

    Done

---
+++ M_systemes_distribues_&&_Services/TPS/TP2/corriger/ex02/Banque.java
@@ -0,0 +1,32 @@
+/*
+
+final double[] comptes  
+----------------------------------
+Banque (int, double)
+double soldeTotal()
+void transferer(in,int,double)
+int size()
+---------------------------------
+*/
+
+public class Banque
+{
+    private final double[] comptes;
+    private int sold;
+
+    public Banque(int sold, double comptes)
+    {
+        this.comptes = comptes;
+        this.sold = sold;
+    }
+
+    public double soldeTotal()
+    {
+        return 
+    }
+
+    int size()
+    {
+        return 
+    }
+}
---
+++ M_systemes_distribues_&&_Services/TPS/TP2/corriger/ex02/Transfert.java
@@ -0,0 +1,13 @@
+public class Transfert implimantes Runnbale
+{
+    private double sold;
+    private final Comptes cmp;
+    private double index;
+
+    
+    double 
+    void run()
+    {
+
+    }
+}
