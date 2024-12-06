80e3fc703191addfc119b05a8acd2b111e964ca8
Goodman <=>
2024-12-06 21:02:46 +0100

    Done

--- M_systemes_distribues_&&_Services/TPS/TP2/corriger/ex01/Main.java
+++ M_systemes_distribues_&&_Services/TPS/TP2/corriger/ex01/Main.java
@@ -1,9 +1,10 @@
 public class Main
 {
 
-	public static void main(String[] args) {
-    	CompteBanque compte = new CompteBanque();
-    	int sold = compte.getSold();
+	public static void main(String[] args) 
+	{
+    CompteBanque compte = new CompteBanque();
+    int sold = compte.getSold();
 	System.out.println("sold now " + sold);
 	compte.setSold(101);
 	sold = compte.getSold();
@@ -11,24 +12,22 @@ public class Main
 
 
    	SanjiEtNamijob job1 = new SanjiEtNamijob(100, "Sanji");
-    	SanjiEtNamijob job2 = new SanjiEtNamijob(100, "Namij");
+    SanjiEtNamijob job2 = new SanjiEtNamijob(100, "Namij");
 
-    	job1.cmp = compte;
-    	job2.cmp = compte;
+    job1.cmp = compte;
+    job2.cmp = compte;
 
-    	Thread thread1 = new Thread(job1);
-    	Thread thread2 = new Thread(job2);
+    Thread thread1 = new Thread(job1);
+    Thread thread2 = new Thread(job2);
 
-    	thread1.start();
-    	thread2.start();
-
-    	try {
-        	thread1.join();
-       	 	thread2.join();
-    	} catch (InterruptedException e) {
-        e.printStackTrace();
-    	}
-
+    thread1.start();
+    thread2.start();
+    try {
+        thread1.join();
+       	thread2.join();
+    } catch (InterruptedException e) {
+    	e.printStackTrace();
+    }
     	System.out.println("Final balance: " + compte.getSold());
 	}
 }
--- M_systemes_distribues_&&_Services/TPS/TP3/corriger/ex01/Client.java
+++ M_systemes_distribues_&&_Services/TPS/TP3/corriger/ex01/Client.java
@@ -6,15 +6,11 @@ public class Client {
         try {
             Socket socket = new Socket("localhost", 12345);
             System.out.println("Connected to server!");
-
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             output.println("Hello from client!");
-
             String serverResponse = input.readLine();
             System.out.println("Received from server: " + serverResponse);
-
             socket.close();
         } catch (IOException e) {
             System.err.println("Client error: " + e.getMessage());
